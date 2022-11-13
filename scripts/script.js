d8a9dc358d7482155602a082e709943c9b614bba
Emil2273 <95055491+Emil2273@users.noreply.github.com>
2022-11-13 18:56:04 +0100

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,78 @@
 var tab = [
-     { word : "pożądana/ mająca wzięcie lokalizacja" , translation : "saught-after address" },
-    { word : "niewielka odległośc (którą można pokonać pieszo)" , translation : "walking distance" },
+    { word : "myć okna" , translation : "clean the windows" },
+    { word : "gotować obiad" , translation : "cook dinner" },
+    { word : "wykonywać prace domowe" , translation : "do chores" },
+    { word : "robić pranie" , translation : "do the laundry" },
+    { word : "odkurzać meble" , translation : "dust the furniture" },
+    { word : "nakrywać do stołu" , translation : "lay the table" },
+    { word : "odgrzać, podgrzać (jedzenie)" , translation : "reheat" },
+    { word : "szorować toaletę" , translation : "scrub the toilet" },
+    { word : "posprzątać bałagan" , translation : "tidy up the mess" },
+    { word : "odkurzyć podłogę" , translation : "vacuum the floor" },
+    { word : "podlać kwiaty" , translation : "water the plants" },
+    { word : "kupować artykuły spożywcze" , translation : "buy groceries" },
+    { word : "kosić trawę" , translation : "cut the grass" },
+    { word : "składać ubrania" , translation : "fold the clothes" },
+    { word : "kosiarka do trawy" , translation : "lawnmower" },
+    { word : "sznurek do suszenia prania" , translation : "line" },
+    { word : "przecierać ściereczką różne powierzchnie" , translation : "wipe surfaces" },
+    { word : "umeblowany pokój" , translation : "furnisched room" },
+    { word : "zakwaterowanie, mieszkanie" , translation : "accommodation" },
+    { word : "wyposażenie, udogodnienia" , translation : "amenities" },
+    { word : "umówić się z kimś w celu obejrzenia (np. mieszkania)" , translation : "arrange a viewing" },
+    { word : "przyległy" , translation : "attached" },
+    { word : "dostępny" , translation : "available" },
+    { word : "położony w centrum" , translation : "centrally located" },
+    { word : "część wspólna, obszar wspólny (dla mieszkańców)" , translation : "common area" },
+    { word : "dojeżdżac (np. do pracy)" , translation : "commute" },
+    { word : "przytulny" , translation : "cosy" },
+    { word : "ciasny" , translation : "cramped" },
+    { word : "zabudowany, na wymiar" , translation : "fitted" },
+    { word : "kuchnia zabudowana" , translation : "fitted kitchen" },
+    { word : "współlokator" , translation : "roommate" },
+    { word : "właścicielka (mieszkania, domu)" , translation : "landlady" },
+    { word : "właściciel (mieszkania, domu)" , translation : "landlord" },
+    { word : "położony" , translation : "located" },
+    { word : "luksusowy" , translation : "luxurious" },
+    { word : "brudny, nieposprzątany, zabałaganiony" , translation : "messy" },
+    { word : "nowoczesny" , translation : "modern" },
+    { word : "umowa miesięczna" , translation : "monthly contract" },
+    { word : "wynegocjować (lepsze warunki)" , translation : "negotiate" },
+    { word : "nowo wybudowany" , translation : "newly built" },
+    { word : "hałaśliwy" , translation : "noisy" },
+    { word : "staromodny, staroświecki" , translation : "old-fashioned" },
+    { word : "prywatny, osobisty" , translation : "private" },
+    { word : "cichy" , translation : "quiet" },
+    { word : "odnowiony" , translation : "renovated" },
+    { word : "wynajmować" , translation : "rent" },
+    { word : "mieszkalny (teren, obszar)" , translation : "residential" },
+    { word : "wspólny, użytkowany przez kilka osób" , translation : "shared" },
+    { word : "podpisac umowę" , translation : "sign the contract" },
+    { word : "przestronny" , translation : "spacious" },
+    { word : "czynsz" , translation : "the rent" },
+    { word : "schludny, uporządkowany" , translation : "tidy" },
+    { word : "media (np. prąd, gaz" , translation : "utilities" },
+    { word : "przystepny (w przystępnej cenie)" , translation : "affordable" },
+    { word : "elastyczny, podlegający negocjacji" , translation : "flexible" },
+    { word : "umowa najmu" , translation : "lease agreement" },
+    { word : "w ofercie" , translation : "on offer" },
+    { word : "lokator, najemca" , translation : "tenant" },
+    { word : "złożyć wniosek o kredyt hipoteczny" , translation : "apply for a mortgage" },
+    { word : "pomagać (komuś w czymś)" , translation : "assist" },
+    { word : "kupujący" , translation : "buyer" },
+    { word : "chętny/skory (do sprzedania czegoś)" , translation : "eager" },
+    { word : "agent nieruchomości" , translation : "estate agent" },
+    { word : "parapetówka" , translation : "house-warming party" },
+    { word : "zainwestować w coś" , translation : "invest in sth" },
+    { word : "licencjonowany" , translation : "licensed" },
+    { word : "złożyc ofertę" , translation : "make an offer" },
+    { word : "dzien otwarty (dla potencjalnych kupców)" , translation : "open house" },
+    { word : "przedział cenowy, widełki cenowe" , translation : "price range" },
+    { word : "nabyć" , translation : "purchase" },
+    { word : "pokazać komiś coś (np. nowy dom)" , translation : "show sb around" },
+    { word : "wykwalifikowany" , translation : "skilled" },
+    { word : "pożądana/ mająca wzięcie lokalizacja" , translation : "saught-after address" },
+    { word : "niewielka odległośc (którą można pokonać pieszo" , translation : "walking distance" },
     { word : "gromadzić (np. kampitał)" , translation : "amass" },
     { word : "historia kredytu" , translation : "credit history" },
     { word : "zadatek, przedpłata" , translation : "down payment" },
@@ -12,10 +84,9 @@ var tab = [
     { word : "folia bąbelkowa" , translation : "bubble wrap" },
     { word : "uprzątnąć (coś)" , translation : "clear" },
     { word : "klient" , translation : "customer" },
-    { word : "pakować się" , translation : "do the packing" },
+    { word : "pakowac się" , translation : "do the packing" },
     { word : "delikatny/kruchy przedmiot" , translation : "fragile item" },
     { word : "oddać coś komuć (coś niepotrzebnego)" , translation : "give sth away to sb" },
-    
     { word : "wynająć" , translation : "hire" },
     { word : "biuro leasingowe" , translation : "leasing office" },
     { word : "brudzić" , translation : "mess" },
@@ -23,11 +94,10 @@ var tab = [
     { word : "wprowadzać się (do mieszkania lub domu)" , translation : "move in" },
     { word : "osoba świadcząca usługi przeprowadzkowe" , translation : "mover" },
     { word : "przedmiot ponadwymiarowy/ponadgabarytowy" , translation : "oversize item" },
-    { word : "pakować" , translation : "pack" },
+    { word : "pakowac" , translation : "pack" },
     { word : "tylne drzwi" , translation : "rear door" },
     { word : "przenosić się (do pracy w innej miejscowości, zmienić miejsce pracy)" , translation : "relocate" },
     { word : "przeprowadzka, przenosiny" , translation : "removal" },
-    
     { word : "firma przeprowadzkowa" , translation : "removal company" },
     { word : "przejrzeć, przeglądać (np. dokumenty)" , translation : "sort through" },
     { word : "zapakować, zawinąć" , translation : "wrap up" },
@@ -39,6 +109,8 @@ var tab = [
     { word : "wypowiedzenie, wymówienie, zawiadomienie" , translation : "notice" },
     { word : "wspólny ogród" , translation : "shared garden" },
     { word : "podpisywać umowę najmu" , translation : "sign a lease" },
+
+    
 ]
 
 var answer_box = document.getElementById("answer_text")
@@ -52,18 +124,7 @@ var incorrect_score = document.getElementById("incorrect_score")
 var los = Math.floor(Math.random()*tab.length)
 word.innerHTML=(tab[los].word)
 
-// Function that draws a new word
-function shuffle() {
-    if (tab.length == 0){
-        word.style.color = "green"
-        word.innerHTML = ("Good! You know all the words!")
-        //correct_score.innerText = 0
-        //incorrect_score.innerText = 0
-        return
-    }
-    los = Math.floor(Math.random()*tab.length)
-    word.innerHTML = (tab[los].word)
-}
+
 
 // Function for checking the correctness of the answer
 answer_box.addEventListener("keydown", function(event) {
@@ -85,7 +146,7 @@ answer_box.addEventListener("keydown", function(event) {
   })
 
 // Function for update the score bar
-  function points(x) {
+function points(x) {
     if (x == 1) {
         var c = correct_score.innerText 
         c = Number(c) + 1
@@ -97,3 +158,17 @@ answer_box.addEventListener("keydown", function(event) {
         incorrect_score.innerText = Number(ic)
     }
 }
+
+// Function that draws a new word
+function shuffle() {
+    if (tab.length == 0){
+        word.style.color = "green"
+        word.innerHTML = ("Good! You know all the words!")
+        //correct_score.innerText = 0
+        //incorrect_score.innerText = 0
+        return
+    }
+    los = Math.floor(Math.random()*tab.length)
+    word.innerHTML = (tab[los].word)
+}
+
